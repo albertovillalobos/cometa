909610851746660b7309d29d1159462f3854fc06
Alberto Villalobos <alberto@albertovillalobos.me>
2014-06-04 15:17:14 -0700

    added view stuff

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,7 +2,13 @@
 
 angular.module('cometaApp')
   .controller('MainCtrl', function ($scope, $http) {
-    $http.get('/api/awesomeThings').success(function(awesomeThings) {
+
+
+    $http.get('/api/items').success(function(awesomeThings) {
       $scope.awesomeThings = awesomeThings;
+      $scope.awesomeThings = $scope.awesomeThings.slice(1,20);
     });
+
+
+
   });
