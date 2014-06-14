ed57028de5598562526c9f8fb2d85251ace62575
Alberto Villalobos <alberto@albertovillalobos.me>
2014-06-14 15:14:12 -0700

    added random item to shopping cart

--- app/scripts/controllers/order.js
+++ app/scripts/controllers/order.js
@@ -3,12 +3,32 @@
 angular.module('cometaApp')
   .controller('OrderCtrl', function ($scope, $http) {
 
+  	// Shopping cart object
+  	$scope.shoppingCart = { items: [], total:0 };
+  	// var car = { myCar: "Saturn", getCar: CarTypes("Honda"), special: Sales };
+  	// Shopping cart items
+  	// $scope.shoppingCart.items = [];
+  	// $scope.shoppingCart.total = 0;
+
 
     $http.get('/api/items').success(function(awesomeThings) {
       $scope.awesomeThings = awesomeThings;
-      // console.log($scope.awesomeThings.length);
-      $scope.awesomeThings = $scope.awesomeThings.slice(1,100);
+      // $scope.awesomeThings = $scope.awesomeThings.slice(1,100);
+      // console.log($scope.awesomeThings);
     });
+
+
+
+    $scope.addRandomItem = function() {
+    	var rand = $scope.awesomeThings[Math.floor(Math.random() * $scope.awesomeThings.length)];
+    	$scope.shoppingCart.items.push(rand);
+    	console.log($scope.shoppingCart.items);
+
+    }
+
+
+
+
 
     
 
